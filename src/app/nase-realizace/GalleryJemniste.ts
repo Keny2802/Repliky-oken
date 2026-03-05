f3c9ba70f016eca7044d20679421332ae9b2b081
Keny2802 <oliva.vojta@gmail.com>
2026-03-05 10:26:22 +0100

    Úprava a aodstranění fota.¨

--- src/app/nase-realizace/GalleryJemniste.ts
+++ src/app/nase-realizace/GalleryJemniste.ts
@@ -1,9 +1,9 @@
 export const GalleryJemniste = [
-    {
-        title: "Jemniště repliky jednorámových oken",
-        image: "/assets/fotky/reference/jemniste-repliky-jednoramovych-oken/jemniste-repliky-jednoramovych-oken-1.avif",
-        alt: "Ukázka práce Jemniště repliky jednorámových oken | Jaroslav Heidinger | replikyoken.cz"
-    },
+    // {
+    //     title: "Jemniště repliky jednorámových oken",
+    //     image: "/assets/fotky/reference/jemniste-repliky-jednoramovych-oken/jemniste-repliky-jednoramovych-oken-1.avif",
+    //     alt: "Ukázka práce Jemniště repliky jednorámových oken | Jaroslav Heidinger | replikyoken.cz"
+    // },
     {
         title: "Jemniště repliky jednorámových oken",
         image: "/assets/fotky/reference/jemniste-repliky-jednoramovych-oken/jemniste-repliky-jednoramovych-oken-2.avif",
