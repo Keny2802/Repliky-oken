1acdd4d5a0e0c7192cfdfb9595f8eae121f4d9aa
Keny2802 <oliva.vojta@gmail.com>
2026-03-08 04:23:29 +0100

    Přidání dlaždice do /nase-realizace.

--- src/app/nase-realizace/Content.tsx
+++ src/app/nase-realizace/Content.tsx
@@ -1,6 +1,10 @@
 import {
     Fragment
 } from "react";
+import {
+    ChevronRight
+} from "lucide-react";
+import Link from "next/link";
 
 import ContactHeader from "../components/contact-header/ContactHeader";
 import Header from "../components/Header";
@@ -17,8 +21,52 @@ import StreetNerudova from "../components/StreetNerudova";
 import LomenaOknaZizkov from "../components/lomenaOknaZizkov";
 import Jemniste from "../components/Jemniste";
 import Pysely from "../components/Pysely";
+import MarginTop from "../components/MarginTop";
+import Flex from "../components/Flex";
+import RelativeOverlayWrapper from "../components/RelativeOverlayWrapper";
+import SmallerHeading from "../components/SmallerHeading";
+import Img from "../components/Img";
 import Contact from "../sections/Contact";
 import Footer from "../sections/Footer";
+
+const realizace = [
+    {
+        image: "/assets/fotky/reference/praha-ulice-rybna-repliky-oken/praha-ulice-rybna-repliky-oken-1.avif",
+        alt: "Ukázka práce špaletových oken v ulice Rybná Prahy 1 | Jaroslav Heidinger | replikyoken.cz",
+        title: "Praha ulice Rybná repliky oken",
+        href: "/nase-realizace/praha-ulice-rybna-repliky-oken"
+    },
+    {
+        image: "/assets/fotky/reference/okna-hradesinska-praha-10/okna-hradesinska-praha-10-2.avif",
+        alt: "Ukázka práce oken ulice Hradešínská na Praze 10 | Jaroslav Heidinger | replikyoken.cz",
+        title: "Praha ulice Hradešínská, Praha 10. Repliky",
+        href: "/nase-realizace/praha-ulice-hradesinska-praha-10-repliky"
+    },
+    {
+        image: "/assets/fotky/reference/jednoramova-okna-praha-ulice-nerudova/jednoramova-okna-praha-ulice-nerudova-1.avif",
+        alt: "Ukázka práce jednorámová okna Praha ulice Nerudova | Jaroslav Heidinger | replikyoken.cz",
+        title: "Jednorámová okna Praha ulice Nerudova",
+        href: "/nase-realizace/jednoramova-okna-praha-ulice-nerudova"
+    },
+    {
+        image: "/assets/fotky/reference/lomena-okna-praha-zizkov/lomena-okna-praha-zizkov-1.avif",
+        alt: "Ukázka práce lomená okna Praha žižkov | Jaroslav Heidinger | replikyoken.cz",
+        title: "Lomená okna Praha Žižkov",
+        href: "/nase-realizace/lomena-okna-praha-zizkov"
+    },
+    {
+        image: "/assets/fotky/reference/jemniste-repliky-jednoramovych-oken/jemniste-repliky-jednoramovych-oken-9.avif",
+        alt: "Ukázka práce Jemniště repliky jednorámových oken | Jaroslav Heidinger | replikyoken.cz",
+        title: "Jemniště repliky jednorámových oken",
+        href: "/nase-realizace/jemniste-repliky-jednoramovych-oken"
+    },
+    {
+        image: "/assets/fotky/reference/pysely-rekonstrukce-historicke-vily/pysely-rekonstrukce-historicke-vily-19.avif",
+        alt: "Ukázka práce rekonstrukce historické vily v Pyšelech | Jaroslav Heidinger | replikyoken.cz",
+        title: "Pyšely rekonstrukce historické vily",
+        href: "/nase-realizace/pysely-rekonstrukce-historicke-vily"
+    }
+];
 
 const Content = () => {
     return (
@@ -36,14 +84,73 @@ const Content = () => {
                             Podívejte se na naše realizace.
                         </Subheading>
                     </FlexCol>
-                    <FlexCol>
+                    {/* <FlexCol>
                         <StreetRybna />
                         <StreetHradesinska />
                         <StreetNerudova />
                         <LomenaOknaZizkov />
                         <Pysely />
                         <Jemniste />
-                    </FlexCol>
+                    </FlexCol> */}
+                    <MarginTop>
+                            <Flex className="justify-center items-center flex-wrap gap-4 md:gap-5 lg:gap-6">
+                                {
+                                    realizace.map((card, index) => {
+                                        const words = card.title.split(" ");
+                                        const highlightedSentence = words.map((word, index) => {
+                                            if (index === words.length - 1) {
+                                                return (
+                                                    <span
+                                                    key={index}
+                                                    className="text-[#e40209]">
+                                                        {word}{" "}
+                                                    </span>
+                                                );
+                                            } else {
+                                                return (
+                                                    <span key={index}>
+                                                        {word}{" "}
+                                                    </span>
+                                                );
+                                            };
+                                        });
+
+                                        return (
+                                            <Link
+                                            key={index}
+                                            href={card.href}
+                                            target="_blank">
+                                                <RelativeOverlayWrapper
+                                                    // key={index}
+                                                    className="p-4 md:p-5 lg:p-6 md:w-[449px] max-h-[600px] odd:bg-[#f2f0ef] even:border-4 even:border-[#f2f0ef] cursor-pointer group">
+                                                        <Wrapper className="transition-transform duration-300 ease-in-out group-hover:scale-105">
+                                                            <SmallerHeading className="text-[#1d1b21] text-center">
+                                                                {/* {card.title} */}
+                                                                {highlightedSentence}
+                                                            </SmallerHeading>
+                                                            <MarginTop>
+                                                                <RelativeOverlayWrapper>
+                                                                    <Img
+                                                                    width={327}
+                                                                    height={360}
+                                                                    src={card.image}
+                                                                    alt={card.alt}
+                                                                    // w-full max-h-[300px] md:w-[327px] md:h-[360px]
+                                                                    className="md:w-[327px] md:h-[360px] mx-auto object-cover"
+                                                                    />                                                            
+                                                                    <Wrapper className="p-2.5 md:p-3 lg:p-4 absolute bottom-5 right-5 md:right-0 bg-[#e40209] text-white rounded-md cursor-pointer">
+                                                                        <ChevronRight className="w-6 h-6 md:w-7 md:h-7 lg:w-8 lg:h-8" />
+                                                                    </Wrapper>
+                                                                </RelativeOverlayWrapper>
+                                                        </MarginTop>
+                                                    </Wrapper>
+                                                </RelativeOverlayWrapper>
+                                            </Link>
+                                        );
+                                    })
+                                }
+                            </Flex>
+                        </MarginTop>
                 </ScrollAnimation>
             </Wrapper>
             <Atc />
--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -12,6 +12,7 @@ import {
     StarIcon
 } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 import Wrapper from "../components/Wrapper";
 import ScrollAnimation from "../components/ScrollAnimation";
@@ -113,7 +114,7 @@ const Hero = () => {
                 className={`w-full min-h-[600px] max-h-[800px] bg-fixed bg-center object-cover transition-opacity duration-500 ease-in-out ${carouselFade ? "opacity-0" : "opacity-100"}`}
                 />
                 <Wrapper className="absolute inset-0 bg-black/40"></Wrapper>
-                <Wrapper className="p-3 md:p-3.5 lg:p-4 text-white text-center absolute inset-0 flex justify-center items-center flex-col gap-3 md:gap-3.5 lg:gap-6">
+                <Wrapper className="p-3 md:p-3.5 lg:p-6 text-white text-center absolute inset-0 flex justify-center items-center flex-col gap-3 md:gap-3.5 lg:gap-6">
                     <ScrollAnimation className="flex justify-center items-center flex-col gap-2 md:gap-4 lg:gap-6">
                        <Wrapper className={`${carouselFade ? "opacity-0" : "opacity-100"} transition-opacity duration-500 ease-in-out`}>
                             <HeroHeading>
@@ -127,7 +128,7 @@ const Hero = () => {
                                                         <Wrapper className="flex items-center gap-1">
                                                             <CheckIcon className="w-8 h-8 text-[#f5f2e8]" />
                                                             <p
-                                                            className="text-xl font-medium max-w-3xl">
+                                                            className="text-base md:text-xl max-w-3xl">
                                                                 {title}
                                                             </p>
                                                         </Wrapper>
@@ -154,7 +155,7 @@ const Hero = () => {
                                             },
                                             {
                                                 text: "Podívat se na recenze",
-                                                href: "//1url.cz/jeJe1"
+                                                href: "//1url.cz/BeeLq"
                                             },
                                             {
                                                 text: "Získat nabídku",
@@ -184,7 +185,7 @@ const Hero = () => {
                                                     //     )
                                                     // }
                                                     // className="w-full bg-[#f5f2e8] text-black last:bg-transparent last:text-white last:border last:border-[#f5f2e8] last:hover:bg-[#f5f2e8] last:hover:text-black transition-colors duration-300 ease-in-out"
-                                                    className="w-full bg-[#f5f2e8] text-black transition-colors duration-300 ease-in-out"
+                                                    className="w-full odd:bg-[#f5f2e8] odd:text-black even:bg-[#87ceeb] even:text-white transition-colors duration-300 ease-in-out"
                                                     // className="bg-[#f5f2e8] text-black transition-colors duration-300 ease-in-out"
                                                     >
                                                         <Padding>
@@ -232,29 +233,33 @@ const Hero = () => {
                                 </Flex>
                             </MarginTop>
                             <MarginTop>
-                                <Flex className="absolute bottom-4 left-4">
-                                    <Img
-                                    width={45}
-                                    height={45}
-                                    src="/assets/google.svg"
-                                    alt="Google ikonka"
-                                    />
-                                    <Flex>
-                                        <BodyText className="font-medium">
-                                            5,0
-                                        </BodyText>
+                                <Link
+                                href="https://1url.cz/BeeLq"
+                                target="_blank">
+                                    <Flex className="absolute bottom-4 left-4">
+                                        <Img
+                                        width={45}
+                                        height={45}
+                                        src="/assets/google.svg"
+                                        alt="Google ikonka"
+                                        />
                                         <Flex>
-                                            <StarIcon className="w-5 h-5 text-yellow-400" />
-                                            <StarIcon className="w-5 h-5 text-yellow-400" />
-                                            <StarIcon className="w-5 h-5 text-yellow-400" />
-                                            <StarIcon className="w-5 h-5 text-yellow-400" />
-                                            <StarIcon className="w-5 h-5 text-yellow-400" />
+                                            <BodyText className="font-medium">
+                                                5,0
+                                            </BodyText>
+                                            <Flex>
+                                                <StarIcon className="w-5 h-5 text-yellow-400" />
+                                                <StarIcon className="w-5 h-5 text-yellow-400" />
+                                                <StarIcon className="w-5 h-5 text-yellow-400" />
+                                                <StarIcon className="w-5 h-5 text-yellow-400" />
+                                                <StarIcon className="w-5 h-5 text-yellow-400" />
+                                            </Flex>
+                                            <BodyText className="font-medium">
+                                                16 recenzí
+                                            </BodyText>
                                         </Flex>
-                                        <BodyText className="font-medium">
-                                            16 recenzí
-                                        </BodyText>
                                     </Flex>
-                                </Flex>
+                                </Link>
                             </MarginTop>
                        </Wrapper>
                     </ScrollAnimation>
